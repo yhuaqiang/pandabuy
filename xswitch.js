5f3e8c0c15b0b116ce2b53d6ee41c5c545ef5f86
yhuaqiang <yhuaqiang@163.com>
2023-11-10 20:21:19 +0800

    1

--- xswitch.js
+++ xswitch.js
@@ -2,21 +2,15 @@
     "proxy": [
         [
             "https://assets.geilicdn.com/v-components/peckerwood/1.6.4/index.js",
-            // "http://fastly.jsdelivr.net/gh/zhaoyangsoft/git-test/gitTest/o-001.js",
-            "https://fastly.jsdelivr.net/gh/yhuaqiang/xswitch@0.0.3/step01/o-001.js",
+            "https://fastly.jsdelivr.net/gh/yhuaqiang/xswitch@0.0.3/step01/o-001.js"
         ],
-        // [
-        // "https://s.geilicdn.com/weidian-h5/order/order-list/index.11cbc7367edb55465e2e.js",
-        // "https://fastly.jsdelivr.net/gh/zhaoyangsoft/git-test/gitTest/b-004.js"
-        // ],
         [
             "https://s.geilicdn.com/node/order-Server-Mixed/order-list/index.7a1a19c2d3a96d0c6224.js",
-            // "https://fastly.jsdelivr.net/gh/zhaoyangsoft/git-test/gitTest/b-004.js",
-            "https://fastly.jsdelivr.net/gh/yhuaqiang/xswitch@0.0.4/step01/index.20231008.js",
+            "https://fastly.jsdelivr.net/gh/yhuaqiang/xswitch@0.0.4/step01/index.20231008.js"
         ],
         [
             "https://admin.pandabuy.com/static/js/app.8028860b.js",
-            "https://fastly.jsdelivr.net/gh/yhuaqiang/xswitch@0.0.7/step02/app.20231110.js",
-        ],
-    ],
+            "https://fastly.jsdelivr.net/gh/yhuaqiang/xswitch@0.0.7/step02/app.20231110.js"
+        ]
+    ]
 }
